b69d48dafce67fc4e3724e542c3eaf5f54bade84
TheMoon2000 <jiarui.shan@berkeley.edu>
2022-10-15 03:10:55 -0700

    Initial Commit

---
+++ Room/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Room
+//
+//  Created by Jia Rui Shan on 2022/10/15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Room/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Room
+//
+//  Created by Jia Rui Shan on 2022/10/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
